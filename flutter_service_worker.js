aac0e5d2ffb005876d9bcd8f50f0fae46dfb418c
Platon Efstathiadis <platonef@gmail.com>
2021-05-21 03:39:45 +0200

    7

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,13 +4,13 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "01df233123f914e1f9bbf00b66927234",
-"main.dart.js": "888c12e2471ee29b657b46b77713036f",
+"main.dart.js": "21c02e16c6ecbb56701ec828a26f51d9",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "manifest.json": "96a70a28840b636fa69aadd964a6548c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "386c34ae7918a812c96cbe1d7f578aa1",
-"/": "386c34ae7918a812c96cbe1d7f578aa1",
+"index.html": "4cc89697c95e0bfe203435ba936b32ad",
+"/": "4cc89697c95e0bfe203435ba936b32ad",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/images/alex-antoniadis-ZtjOCn16gMQ-unsplash.jpg": "c8245cb895a444d8091a38648162abb1",
 "assets/images/people-at-the-station-3371242.jpg": "bab82ec3e3757335d48c73b1b4898cfc",
